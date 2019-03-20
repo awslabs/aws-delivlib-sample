0aa2e5fdd6584ca9b65c937b9b21b5f53ca74a1a
Elad Ben-Israel <benisrae@amazon.com>
2019-03-20 15:56:34 +0200

    add python!

--- lib/index.ts
+++ lib/index.ts
@@ -3,20 +3,20 @@ export interface HelloJsiiProps {
      * The message to emit when saying goodbye.
      * @default "Goodbye"
      */
-    goodbyeMessage?: string;
+    readonly goodbyeMessage?: string;
 }
 
 /**
  * This guy knows how to say hello, in multiple langauges.
  */
 export class HelloJsii {
-    
+
     private readonly goodbyeMessage: string;
-    
+
     constructor(props: HelloJsiiProps = {}) {
         this.goodbyeMessage = props.goodbyeMessage || 'Goodbye';
     }
-    
+
     /**
      * Say hello to someone.
      * @param name That special someone
@@ -24,7 +24,7 @@ export class HelloJsii {
     public sayHello(name: string) {
         return `Hello, ${name}!!`;
     }
-    
+
     /**
      * Says goodbye, but not farewell.
      * @param times Number of times to say goodbye.
